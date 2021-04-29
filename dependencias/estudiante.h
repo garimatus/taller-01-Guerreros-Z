44fc5d4dc801729c00f43777f29560d2588553a4
root <matus.edgar@gmail.com>
2021-04-29 15:23:50 -0400

    ADD: estudiante.h

---
+++ dependencias/estudiante.h
@@ -0,0 +1,12 @@
+struct estudiante {
+    int id_numerico;
+    const char* id_generico;
+    double prom_len;
+    double prom_ing;
+    double prom_mat;
+    double prom_sci;
+    double prom_his;
+    double prom_tec;
+    double prom_art;
+    double prom_edf;
+};
