f417df72fb61268da05379dd7b27438d23afde97
root <matus.edgar@gmail.com>
2021-04-29 15:25:54 -0400

    lectura

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+#include "dependencias/funciones.h"
+
+
+
+int main(int argc, char** argv)
+{
+    std::string ruta = argv[1];
+
+    std::ifstream lectura;
+    lectura.open(ruta, std::ios_base::in);
+
+    if (lectura && std::string(ruta.substr(ruta.length()-15, 15)) == "estudiantes.csv")
+    {
+        std::vector<estudiante> notas = obtenerNotas(lectura);
+
+    } else {
+        
+        std::cout << "El archivo solicitado 'estudiantes.csv' no existe o no se encuentra en el directorio ingresado." << std::endl;
+
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
+}
